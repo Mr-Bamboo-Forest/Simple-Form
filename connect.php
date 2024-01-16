b3bda32a73392b5abbbbede349922aa5bd1fa318
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2024-01-16 15:09:13 +1000

    new

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+	$full_name = $_POST['full_name'];
+    $message = $_POST['message'];
+    $email = $_POST['email'];
+	$gender = $_POST['gender'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
