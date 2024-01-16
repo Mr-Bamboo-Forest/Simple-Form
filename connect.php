e50565dd603e5f2522aae23090de1792b01d5f28
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2024-01-16 16:30:51 +1000

    sd

--- connect.php
+++ connect.php
@@ -1,21 +1,15 @@
-<?php
-	$full_name = $_POST['full_name'];
-    $message = $_POST['message'];
-    $email = $_POST['email'];
-	$gender = $_POST['gender'];
+<!DOCTYPE html>
+<html>
+	<head>
+        <title>Simple Form</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="pfp.jpg" />
+    </head>
+	<body>
+		<?php 
+		echo '<h1> text is: hi </hi>'
+		?>
+	</body>
+</html>
 
-	// Database connection
-	$conn = new mysqli('localhost','root','','test');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo "Registration successfully...";
-		$stmt->close();
-		$conn->close();
-	}
-?>
+
